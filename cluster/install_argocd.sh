b24addcd2f061586d6819027975f027dad2bec80
Marlon Araujo <marlonaraujo2709@gmail.com>
2024-04-21 21:40:44 -0300

    ADD: argocd install manifest

--- cluster/install_argocd.sh
+++ cluster/install_argocd.sh
@@ -1,3 +1,6 @@
 kubectl create ns argocd
+
+# kubectl apply -n argocd -f argocd_manifest.yaml
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 # kubectl create -f argocd_svc.yaml
