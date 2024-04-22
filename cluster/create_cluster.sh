dcf2f8da5c4eb9cf194a31365fbb6aef37e4eeb5
Marlon Araujo <marlonaraujo2709@gmail.com>
2024-04-21 21:30:15 -0300

    UPDATE: adiciona hashbang no script create_cluster.sh

--- cluster/create_cluster.sh
+++ cluster/create_cluster.sh
@@ -1,3 +1,5 @@
-kind create cluster --config=cluster_config.yaml
-. ./install_argocd.sh
-. ./bootstrap_argo_apps.sh
+#!/bin/bash
+kind create cluster --config=cluster_config.yaml --name projeto2-cluster
+
+. install_argocd.sh
+. bootstrap_argo_apps.sh
